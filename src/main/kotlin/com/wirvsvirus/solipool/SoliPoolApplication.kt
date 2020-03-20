8ddba2d2813d61bba54d4212628906b984959837
Johanna Schmidt <johanna.schmidt@just.social>
2020-03-20 23:02:43 +0100

    basic setup for solipool project
    - used spring initializr to setup basic project
    - added H2, rest and kotlin support
    
    Change-Id: Ibb752d1cb6a000ac124459d08e27ff993c9d98d0

---
+++ src/main/kotlin/com/wirvsvirus/solipool/SoliPoolApplication.kt
@@ -0,0 +1,11 @@
+package com.wirvsvirus.solipool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SoliPoolApplication
+
+fun main(args: Array<String>) {
+	runApplication<SoliPoolApplication>(*args)
+}

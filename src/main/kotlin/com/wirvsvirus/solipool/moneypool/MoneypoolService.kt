69e061bfbec85b0448c95c837c98f51eccebe854
Johanna Schmidt <johanna.schmidt@just.social>
2020-03-22 13:28:17 +0100

    Moneypool
    Wrapped creator request in transaction to get
    right user
    
    Change-Id: I8eccb51b7bfbe114497cda2d88763c961886741b

--- src/main/kotlin/com/wirvsvirus/solipool/moneypool/MoneypoolService.kt
+++ src/main/kotlin/com/wirvsvirus/solipool/moneypool/MoneypoolService.kt
@@ -2,10 +2,10 @@ package com.wirvsvirus.solipool.moneypool
 
 import com.wirvsvirus.solipool.banking.AccountBalanceService
 import com.wirvsvirus.solipool.moneypool.model.Moneypool
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.math.BigDecimal
-import java.math.BigInteger
 import java.util.*
 
 @Service
@@ -15,9 +15,11 @@ class MoneypoolService @Autowired constructor(
 
     fun getMoneypool(id: UUID): Moneypool? {
         val moneypoolRow = moneypoolRepository.getMoneypool(id)
-        val balance = accountBalanceService.getBalanceForPool(id) ?: BigDecimal.ZERO
+        val balance = accountBalanceService.getBalanceForPool(id)
         return if (moneypoolRow != null && balance != null) {
-            Moneypool(moneypoolRow.id.value, moneypoolRow.name, 1, balance)
+            transaction {
+                Moneypool(moneypoolRow.id.value, moneypoolRow.name, moneypoolRow.creator.id.value, balance)
+            }
         } else {
             return null
         }
@@ -25,6 +27,8 @@ class MoneypoolService @Autowired constructor(
 
     fun createMoneypool(name: String, creator: Int): Moneypool {
         val moneypoolRow = moneypoolRepository.createMoneypool(name, creator)
-        return Moneypool(moneypoolRow.id.value, moneypoolRow.name, creator, BigDecimal.ZERO)
+        return transaction {
+            Moneypool(moneypoolRow.id.value, moneypoolRow.name, moneypoolRow.creator.id.value, BigDecimal.ZERO)
+        }
     }
 }
